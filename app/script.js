9ec6c9c6a93fcb8609edd4dcc3d2f550b79f7597
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-06-08 11:01:31 +0300

    implemented creating cards

--- app/script.js
+++ app/script.js
@@ -1,7 +1,48 @@
 $(".cards-container").sortable({
   connectWith: ".cards-container",
-  cursor: "grabbing",
   revert: true,
   revertDuration: 70,
-  scroll: false,
+});
+
+$("textarea")
+  .each(function () {
+    this.setAttribute(
+      "style",
+      "height:" + this.scrollHeight + "px;overflow-y:hidden;"
+    );
+  })
+  .on("input", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+  });
+
+$(".add-new-card").click(function (event) {
+  let listId = event.target.id.slice(-1);
+
+  $(`.create-card-container`).hide();
+
+  $(`#create-card-container${listId}`).show();
+});
+
+$(".create-card-btn").click(function (event) {
+  let listId = event.target.id.slice(-1);
+
+  console.log(`#create-card-input${listId}`);
+  let cardName = $(`#create-card-input${listId}`).val();
+
+  if (cardName) {
+    $(`#card-container${listId}`).append(
+      `<div class="card">
+      <span class="card-text">${cardName}</span>
+      <img src="assets/edit-pencil.svg" class="card-edit" alt="Edit" />
+    </div>`
+    );
+
+    $(`#create-card-container${listId}`).hide();
+    $(".create-card-input").val("");
+  }
+});
+
+$(".create-card-close").click(function () {
+  $(".create-card-container").hide();
 });
